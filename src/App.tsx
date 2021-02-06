331d7ab7a2d09a56611d2bbf6e2113330a672946
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-07 00:25:35 +0900

    U

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from 'react';
+import { getUsers } from './api';
 import './App.css';
 
+const initialState = {
+  users: [],
+  isLoaded: false,
+  error: null,
+};
+
 function App() {
-  return <div></div>;
+  const [state, setState] = useState(initialState);
+
+  const loadUsers = () => {
+    getUsers().then((result) => {
+      setState({
+        ...state,
+        isLoaded: true,
+        users: result,
+      });
+    });
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.users.map((user) => (
+            <tr key={user.id}></tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
