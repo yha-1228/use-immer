077e37197c5ccbec4d6b94c1a9ccd854065ecbbb
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-07 01:03:52 +0900

    Move UserTable to UsrtTable/index.tsx

---
+++ src/UserTable/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import Box from '@material-ui/core/Box'
+import { getUsers } from '../api'
+import { Users } from '../types'
+import { delay } from '../utils'
+
+type State = {
+  users: Users
+  isLoaded: boolean
+  error: string | null
+}
+
+const initialState = {
+  users: [],
+  isLoaded: false,
+  error: null,
+}
+
+export default function UserTable() {
+  const [state, setState] = useState<State>(initialState)
+
+  useEffect(() => {
+    getUsers()
+      .then(async (result) => {
+        await delay(800)
+        setState({
+          ...state,
+          isLoaded: true,
+          users: result,
+        })
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }, [state])
+
+  return (
+    <Box p="16px" m="0 auto" maxWidth="720px" bgcolor="#f5f5f5">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Box>
+  )
+}
